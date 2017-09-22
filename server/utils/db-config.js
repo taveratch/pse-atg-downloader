7f594a3062d319d000910afd2878ef86239e42eb
taweesoft <taweesoft@gmail.com>
2017-09-22 22:23:14 +0700

    use real db

--- server/utils/db-config.js
+++ server/utils/db-config.js
@@ -1,5 +1,5 @@
 export let USERNAME = 'root';
-export let PASSWORD = '';
-export let DB_NAME = 'pse_inventory_downloader';
-export let HOST = 'localhost';
+export let PASSWORD = 'pse@TGdownloader';
+export let DB_NAME = process.env.NODE_ENV === 'production' ? 'pse_inventory_downloader' : 'pse_inventory_downloader_development' ;
+export let HOST = 'sql.padungsilpa.group';
 export let PORT = 3306;
